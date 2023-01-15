5f74e20a18ed6987481535eee2f4b31191b2bd77
johnnysit <37731817+johnnysiit@users.noreply.github.com>
2023-01-15 00:46:40 -0600

    Update price_detect.py

--- price_detect.py
+++ price_detect.py
@@ -12,7 +12,7 @@ def get_price(depart_city, arrive_city, depart_date):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_argument("window-size=1920,1080")
+    chrome_options.add_argument("window-size=1000,600")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
     chrome_options.add_argument('--no-sandbox')
